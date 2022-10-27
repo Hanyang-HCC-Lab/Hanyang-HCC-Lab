e137a45a2d436db6abba4ead3cf9f0717a10764a
Jaehoon Kim <jaehoonkim.dev@gmail.com>
2022-10-27 17:16:11 +0900

    mod nav title

--- frontend/src/components/base/Nav.vue
+++ frontend/src/components/base/Nav.vue
@@ -1,7 +1,11 @@
+<style>
+@import url("https://fonts.googleapis.com/css2?family=Archivo:wght@500&family=IBM+Plex+Sans:wght@600&display=swap");
+</style>
 <template>
   <nav
     class="navbar navbar-expand-lg navbar-dark fixed-top bg-black"
     id="mainNav"
+    style="height: 5.5rem"
   >
     <div class="container">
       <a class="navbar-brand" style="width: 3rem"
@@ -23,8 +27,17 @@
         <i class="fas fa-bars ms-1"></i>
       </button>
       <div class="collapse navbar-collapse" id="navbarResponsive">
-        <p class="fs-3" style="margin: 0; color: white">
-          Hanyang Human-Centered Computing Lab
+        <p
+          class="fs-3"
+          style="
+            margin: 0;
+            color: white;
+            font-family: 'IBM Plex Sans', sans-serif;
+          "
+        >
+          HANYANG
+          <span style="color: rgba(253, 193, 58)">HUMAN-CENTERED</span>
+          COMPUTING LAB
         </p>
         <ul class="navbar-nav text-uppercase ms-auto py-4 py-lg-0">
           <li class="nav-item">
