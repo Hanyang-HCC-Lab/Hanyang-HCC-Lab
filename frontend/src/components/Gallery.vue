43a0d3c21328b2204459675e8c66d4df32fb2fe2
Jaehoon Kim <jaehoonkim.dev@gmail.com>
2022-12-13 16:33:20 +0900

    add CIKM and VRST in Gallery

--- frontend/src/components/Gallery.vue
+++ frontend/src/components/Gallery.vue
@@ -5,6 +5,45 @@
       <div class="col-md-4 text-center mb-3">
         <div class="card img-wrapper">
           <img
+            src="https://s3.ap-northeast-2.amazonaws.com/hcc.hanyang.ac.kr/image/gallery/VRST(22)-1.png"
+            style="max-height: 20rem"
+          />
+          <div class="card-body">
+            <span class="h6 card-text">[2022.11] VRST 2022</span>
+          </div>
+        </div>
+      </div>
+      <!-- End Picture -->
+      <!-- Start Picture -->
+      <div class="col-md-4 text-center mb-3">
+        <div class="card img-wrapper">
+          <img
+            src="https://s3.ap-northeast-2.amazonaws.com/hcc.hanyang.ac.kr/image/gallery/CIKM(22)-2.jpeg"
+            style="max-height: 20rem"
+          />
+          <div class="card-body">
+            <span class="h6 card-text">[2022.10] CIKM 2022</span>
+          </div>
+        </div>
+      </div>
+      <!-- End Picture -->
+      <!-- Start Picture -->
+      <div class="col-md-4 text-center mb-3">
+        <div class="card img-wrapper">
+          <img
+            src="https://s3.ap-northeast-2.amazonaws.com/hcc.hanyang.ac.kr/image/gallery/CIKM(22)-1.jpeg"
+            style="max-height: 20rem"
+          />
+          <div class="card-body">
+            <span class="h6 card-text">[2022.10] CIKM 2022</span>
+          </div>
+        </div>
+      </div>
+      <!-- End Picture -->
+      <!-- Start Picture -->
+      <div class="col-md-4 text-center mb-3">
+        <div class="card img-wrapper">
+          <img
             src="https://s3.ap-northeast-2.amazonaws.com/hcc.hanyang.ac.kr/image/gallery/skcc-1.jpg"
             style="max-height: 20rem"
           />
