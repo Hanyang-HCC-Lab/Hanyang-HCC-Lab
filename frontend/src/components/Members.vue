51816bd0e0864003da7b0c357ddb6b4d80bbf020
jaehoonkimm <jaehoonkim.dev@gmail.com>
2023-04-13 11:46:42 +0900

    mod title of undergraduate students tap to research interns and move JH Kim to PhD student

--- frontend/src/components/Members.vue
+++ frontend/src/components/Members.vue
@@ -215,6 +215,41 @@
           <img
             class="rounded-circle mt-3 memberImage"
             fetchpriority="high"
+            src="https://hyhccl.s3.ap-northeast-2.amazonaws.com/image/members/%E1%84%80%E1%85%B5%E1%86%B7%E1%84%8C%E1%85%A2%E1%84%92%E1%85%AE%E1%86%AB.webp"
+          />
+          <div class="text-center mt-1">
+            <p class="member_name" style="font-size: 1rem; margin: 0">
+              Jaehoon Kim
+            </p>
+            <p class="member_name" style="font-size: 1rem; margin: 0">김재훈</p>
+          </div>
+          <!-- Start Personal Link -->
+          <div class="mt-1">
+            <a
+              type="button"
+              class="btn btn-circle btn-dark"
+              href="mailto: jaehoonkimm@hanyang.ac.kr"
+              target="_blank"
+            >
+              <font-awesome-icon icon="fa-solid fa-envelope" size="lg" /> </a
+            >&nbsp;
+            <a
+              type="button"
+              class="btn btn-circle btn-dark"
+              href="https://hyhccl.s3.ap-northeast-2.amazonaws.com/Lab-members-CV/CV_Jaehoon_Kim.pdf"
+              target="_blank"
+            >
+              <font-awesome-icon icon="fa-solid fa-link" size="lg" />
+            </a>
+          </div>
+          <!-- End Personal Link -->
+        </div>
+        <!-- End Member Profile -->
+        <!-- Start Member Profile -->
+        <div class="col-12 col-md-3 col-sm-6 text-center">
+          <img
+            class="rounded-circle mt-3 memberImage"
+            fetchpriority="high"
             src="https://hyhccl.s3.ap-northeast-2.amazonaws.com/image/members/%E1%84%8B%E1%85%A7%E1%84%92%E1%85%A2%E1%84%8B%E1%85%B5%E1%86%AB.webp"
           />
           <div class="text-center mt-1">
@@ -364,41 +399,7 @@
           <!-- End Personal Link -->
         </div>
         <!-- End Member Profile -->
-        <!-- Start Member Profile -->
-        <div class="col-12 col-md-3 col-sm-6 text-center">
-          <img
-            class="rounded-circle mt-3 memberImage"
-            fetchpriority="high"
-            src="https://hyhccl.s3.ap-northeast-2.amazonaws.com/image/members/%E1%84%80%E1%85%B5%E1%86%B7%E1%84%8C%E1%85%A2%E1%84%92%E1%85%AE%E1%86%AB.webp"
-          />
-          <div class="text-center mt-1">
-            <p class="member_name" style="font-size: 1rem; margin: 0">
-              Jaehoon Kim
-            </p>
-            <p class="member_name" style="font-size: 1rem; margin: 0">김재훈</p>
-          </div>
-          <!-- Start Personal Link -->
-          <div class="mt-1">
-            <a
-              type="button"
-              class="btn btn-circle btn-dark"
-              href="mailto: jaehoonkimm@hanyang.ac.kr"
-              target="_blank"
-            >
-              <font-awesome-icon icon="fa-solid fa-envelope" size="lg" /> </a
-            >&nbsp;
-            <a
-              type="button"
-              class="btn btn-circle btn-dark"
-              href="https://hyhccl.s3.ap-northeast-2.amazonaws.com/Lab-members-CV/CV_Jaehoon_Kim.pdf"
-              target="_blank"
-            >
-              <font-awesome-icon icon="fa-solid fa-link" size="lg" />
-            </a>
-          </div>
-          <!-- End Personal Link -->
-        </div>
-        <!-- End Member Profile -->
+
         <!-- Start Member Profile -->
         <div class="col-12 col-md-3 col-sm-6 text-center">
           <img
@@ -578,12 +579,12 @@
     </div>
     <!-- End MS Students Tap -->
 
-    <!-- Start Undergraduate Students Tap -->
+    <!-- Start Research Interns Tap -->
     <div class="container mt-5" style="width: 50%">
       <!-- Stack the columns on mobile by making one full-width and the other half-width -->
       <div class="row-cols-2">
         <span style="letter-spacing: normal"
-          ><span style="font-size: 1.5rem">Undergraduate Students</span></span
+          ><span style="font-size: 1.5rem">Research Interns</span></span
         >
       </div>
       <div class="row row-cols-10">
