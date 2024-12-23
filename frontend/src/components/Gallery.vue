fba3b10853af019e7b88d249ecdd0ab1d07681b3
jaehoonkimm <jaehoonkim.dev@gmail.com>
2024-12-23 14:22:25 +0900

    add KSC 2024 photo

--- frontend/src/components/Gallery.vue
+++ frontend/src/components/Gallery.vue
@@ -5,6 +5,19 @@
 <div class="col-md-4 text-center mb-3">
         <div class="card img-wrapper">
           <img
+            src="https://s3.ap-northeast-2.amazonaws.com/hcc.hanyang.ac.kr/image/gallery/KSC(24).jpg"
+            style="max-height: 20rem"
+          />
+          <div class="card-body">
+            <span class="h6 card-text">[2024.12] KSC 2024</span>
+          </div>
+        </div>
+      </div>
+<!-- End Picture -->
+<!-- Start Picture -->
+<div class="col-md-4 text-center mb-3">
+        <div class="card img-wrapper">
+          <img
             src="https://s3.ap-northeast-2.amazonaws.com/hcc.hanyang.ac.kr/image/gallery/Defense_2412_MS.jpg"
             style="max-height: 20rem"
           />
