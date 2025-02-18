d6199bbc6b05a275a6192c086411795aff28b1af
jaehoonkimm <jaehoonkim.dev@gmail.com>
2025-02-18 13:51:57 +0900

    add HCIK 2025 Photo

--- frontend/src/components/Gallery.vue
+++ frontend/src/components/Gallery.vue
@@ -1,7 +1,20 @@
 <template>
   <div class="container mt-5 text-center" style="width: 100%">
     <div class="row">
-    <!-- Start Picture -->
+<!-- Start Picture -->
+<div class="col-md-4 text-center mb-3">
+        <div class="card img-wrapper">
+          <img
+            src="https://s3.ap-northeast-2.amazonaws.com/hcc.hanyang.ac.kr/image/gallery/HCIK(25).jpg"
+            style="max-height: 20rem"
+          />
+          <div class="card-body">
+            <span class="h6 card-text">[2025.02] HCI Korea 2025</span>
+          </div>
+        </div>
+      </div>
+<!-- End Picture -->
+<!-- Start Picture -->
 <div class="col-md-4 text-center mb-3">
         <div class="card img-wrapper">
           <img
