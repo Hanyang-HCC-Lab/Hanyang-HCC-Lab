ce1efaf1c7038cc99b3c6a4f97cb860bb2a05e88
jaehoonkimm <jaehoonkim.dev@gmail.com>
2025-02-26 19:06:34 +0900

    mod graduation 25 photo

--- frontend/src/components/Gallery.vue
+++ frontend/src/components/Gallery.vue
@@ -5,7 +5,20 @@
 <div class="col-md-4 text-center mb-3">
         <div class="card img-wrapper">
           <img
-            src="https://s3.ap-northeast-2.amazonaws.com/hcc.hanyang.ac.kr/image/gallery/graduation_25.jpg"
+            src="https://s3.ap-northeast-2.amazonaws.com/hcc.hanyang.ac.kr/image/gallery/graduation_25-2.jpg"
+            style="max-height: 20rem"
+          />
+          <div class="card-body">
+            <span class="h6 card-text">[2025.02] Commencement Ceremony</span>
+          </div>
+        </div>
+      </div>
+<!-- End Picture -->
+<!-- Start Picture -->
+<div class="col-md-4 text-center mb-3">
+        <div class="card img-wrapper">
+          <img
+            src="https://s3.ap-northeast-2.amazonaws.com/hcc.hanyang.ac.kr/image/gallery/graduation_25-1.jpg"
             style="max-height: 20rem"
           />
           <div class="card-body">
