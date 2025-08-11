7a15aa882229fa39cf0eda74ea85261cb3b972c9
jaehoonkimm <jaehoonkim.dev@gmail.com>
2025-08-11 15:45:11 +0900

    add photo: 2025 MT

--- frontend/src/components/Gallery.vue
+++ frontend/src/components/Gallery.vue
@@ -5,6 +5,19 @@
 <div class="col-md-4 text-center mb-3">
         <div class="card img-wrapper">
           <img
+            src="https://s3.ap-northeast-2.amazonaws.com/hcc.hanyang.ac.kr/image/gallery/MT-2025.jpg"
+            style="max-height: 20rem"
+          />
+          <div class="card-body">
+            <span class="h6 card-text">[2025.08] MT</span>
+          </div>
+        </div>
+      </div>
+<!-- End Picture -->
+<!-- Start Picture -->
+<div class="col-md-4 text-center mb-3">
+        <div class="card img-wrapper">
+          <img
             src="https://s3.ap-northeast-2.amazonaws.com/hcc.hanyang.ac.kr/image/gallery/KCC2025-2.jpg"
             style="max-height: 20rem"
           />
