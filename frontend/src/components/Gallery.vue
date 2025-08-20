b80df6a2886065de705ab0185dcf5a026d075da2
jaehoonkimm <jaehoonkim.dev@gmail.com>
2025-08-21 00:23:23 +0900

    add photo 2025.08 commencement ceremony

--- frontend/src/components/Gallery.vue
+++ frontend/src/components/Gallery.vue
@@ -5,6 +5,19 @@
 <div class="col-md-4 text-center mb-3">
         <div class="card img-wrapper">
           <img
+            src="https://s3.ap-northeast-2.amazonaws.com/hcc.hanyang.ac.kr/image/gallery/graduation_25-3.jpg"
+            style="max-height: 20rem"
+          />
+          <div class="card-body">
+            <span class="h6 card-text">[2025.08] Commencement Ceremony</span>
+          </div>
+        </div>
+      </div>
+<!-- End Picture -->
+<!-- Start Picture -->
+<div class="col-md-4 text-center mb-3">
+        <div class="card img-wrapper">
+          <img
             src="https://s3.ap-northeast-2.amazonaws.com/hcc.hanyang.ac.kr/image/gallery/MT-2025.jpg"
             style="max-height: 20rem"
           />
